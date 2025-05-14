7ff9776007018bdb9c1cf535fa7255bd5222f269
Maciej Ślusarz <maciej.slusarz@proton.me>
2025-05-14 20:28:46 +0200

    Add proper error handlers instead of raw unwraping

--- src/core/hardware.rs
+++ src/core/hardware.rs
@@ -21,39 +21,39 @@ use crate::config::spec::*;
 
 // Each function currently panics if something goes wrong with hardware
 
-pub fn accelerate(/* current_acc_state: Arc<AtomicBool> */) /* -> Result<(), Box<dyn Error>> */ {
-    let mut accelerator_pin = Gpio::new().unwrap()
-            .get(ACCELERATOR_GPIO).unwrap().into_output();
+pub fn accelerate(/* current_acc_state: Arc<AtomicBool> */) -> Result<(), Box<dyn Error>> {
+    let mut accelerator_pin = Gpio::new()?
+            .get(ACCELERATOR_GPIO)?.into_output();
     info!("Accelerating");
     //current_acc_state.clone().store(true, Ordering::SeqCst);
     accelerator_pin.set_high();
 
-    //Ok(())
+    Ok(())
 }
 
-pub fn stop_acceleration(/* current_acc_state: Arc<AtomicBool> */) /* -> Result<(), Box<dyn Error>> */ {
-    let mut accelerator_pin = Gpio::new().unwrap()
-            .get(ACCELERATOR_GPIO).unwrap().into_output();
+pub fn stop_acceleration(/* current_acc_state: Arc<AtomicBool> */) -> Result<(), Box<dyn Error>> {
+    let mut accelerator_pin = Gpio::new()?
+            .get(ACCELERATOR_GPIO)?.into_output();
     //current_acc_state.clone().store(false, Ordering::SeqCst);
     info!("Stopping the vehicle");
     accelerator_pin.set_low();
 
-    //Ok(())
+    Ok(())
 }
 
-pub fn steer(degrees: u8) /* -> Result<(), Box<dyn Error>> */ {
+pub fn steer(degrees: u8) -> Result<(), Box<dyn Error>> {
     let servo_pin = Pwm::with_period(
         SERVO_GPIO, 
         Duration::from_millis(PERIOD_MS), 
         Duration::from_micros(PULSE_MAX_US), 
         Polarity::Normal, 
         true
-    ).unwrap();
+    )?;
     
     let pulse = (degrees as u64) * ((PULSE_MAX_US - PULSE_MIN_US) / (MAX_DEGREE as u64)) + PULSE_MIN_US;
     
     info!("Set servo to {} degrees", degrees);
-    servo_pin.set_pulse_width(Duration::from_micros(pulse)).unwrap();
+    servo_pin.set_pulse_width(Duration::from_micros(pulse))?;
 
-    //Ok(())
+    Ok(())
 }
